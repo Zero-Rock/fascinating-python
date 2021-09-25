80ea4016bcec8696d435a8f657e6661040ad46a5
Zero <mobius_pan@yeah.net>
2021-09-25 13:10:39 +0800

    fix: modify logic

--- demo/junjin/config.py
+++ demo/junjin/config.py
@@ -3,7 +3,7 @@
 @Time   : 2021/9/22 19:01:06
 """
 
-BASE_URL = "https://api.juejin.cn";
+BASE_URL = "https://api.juejin.cn"
 API_MAP = {
     "todayStatus": {
         'url': "/growth_api/v1/get_today_status",
@@ -29,5 +29,5 @@ API_MAP = {
 
 
 def getConfig(key):
-    temp = API_MAP.get(key);
-    return ("{}{}".format(BASE_URL, temp.get("url")), temp.get('method'))
+    temp = API_MAP.get(key)
+    return f"{BASE_URL}{temp.get('url')}", temp.get('method')
